c8959e056e5ea82ec2774ad971b4a3594636b729
MungaiKeren <wambukeren@gmail.com>
2020-12-16 08:41:54 +0300

    Rename the file to matrices.R

---
+++ Matrices.R
@@ -0,0 +1,17 @@
+y <- list(1,"a", TRUE, 4i)
+y
+x <- c(1.7 , "a")
+print(x)
+m <- matrix(1:6, nrow=2, ncol = 3)
+m # creates a 2 by 5 matrix
+
+m <- 1:10
+dim(m) <- c(2,5)
+m
+x <- 1:3
+y <- 10:12
+cbind(x,y)
+rbind(x,y)
+
+
+
