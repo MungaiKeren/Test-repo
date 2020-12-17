007d9fdfffa8d3b4c0448ad344cfa5fb0401041d
MungaiKeren <wambukeren@gmail.com>
2020-12-17 09:37:14 +0300

    Compute the mean of varioues conditions using the subset method and apply

--- quiz test.R
+++ quiz test.R
@@ -17,4 +17,34 @@ tail(csv_data, 2)
 csv_data[47,1]
 csv_data[47, "Ozone"]
 
-# missing values in the ozone column
+# how many missing values in the ozone column
+missing_values = subset(csv_data, is.na(Ozone))
+nrow(missing_values)
+
+# mean of the Ozone column exclude missing values
+# step 1 exclude missing values
+new_Ozone <- csv_data[complete.cases(csv_data),]
+# gets the mean of all columns
+apply(new_Ozone,2,mean)
+
+# method 2
+sub = subset(csv_data, !is.na(Ozone), select = Ozone)
+apply(sub, 2, mean)
+
+
+# Extract the subset of rows of the data frame where,
+# Ozone values are above 31 and Temp values are above 90. 
+sub <- subset(csv_data, Ozone > 31 & Temp > 90, select = Solar.R)
+sub
+# What is the mean of Solar.R in this subset?
+apply(sub, 2, mean)
+
+#What is the mean of temp when month is equal to 6
+sub <- subset(csv_data, Month==6, select = Temp)
+sub
+apply(sub,2,mean)
+
+# the maximun Ozone value on May i.e month=5
+sub <- subset(csv_data, Month == 5 & !is.na(Ozone), select = Ozone)
+sub
+apply(sub, 2, max)
